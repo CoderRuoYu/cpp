5b5c740026f7b33184965d558829dda037ebd210
若鱼不是鱼 <21231270@bjtu.edu.cn>
2023-11-18 11:54:05 +0800

    数据结构-堆
    堆的实现

--- Heap/Heap/heap.cpp
+++ Heap/Heap/heap.cpp
@@ -1,2 +1,64 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include "heap.h"
+void AdjustDown(HPDataType* a, int size, int parents)
+{
+	int child = parents * 2 + 1;
+	while (child < size)
+	{
+		if (child + 1 < size && a[child] < a[child + 1])
+		{
+			child++;
+		}
+		//构造大堆
+		if (a[child] > a[parents])
+		{
+			swap(a[child], a[parents]);
+			parents = child;
+			child = parents * 2 + 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+void HeapCreate(Heap* hp, HPDataType* a, int n)
+{
+	hp->_size = n;
+	hp->_capacity = n;
+	hp->_a = new HPDataType[n];
+	memmove(hp->_a, a, n);
+	for (int i = (n - 1 - 1) / 2; i >= 0; i--)
+	{
+		AdjustDown(hp->_a, hp->_size, i);
+	}
+}
+void HeapDestory(Heap* hp)
+{
+	delete[] hp->_a;
+	hp->_a = nullptr;
+	hp->_size = 0;
+	hp->_capacity = 0;
+	hp = nullptr;
+}
+bool HeapEmpty(Heap* hp)
+{
+	assert(hp);
+	if (hp->_size == 0)
+	{
+		return true;
+	}
+	return false;
+}
+int HeapSize(Heap* hp)
+{
+	assert(hp);
+	return hp->_size;
+}
+HPDataType HeapTop(Heap* hp)
+{
+	assert(hp && hp->_size > 0);
+	return hp->_a[0];
+}
+
+
--- Heap/Heap/heap.h
+++ Heap/Heap/heap.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <assert.h>
 #include <iostream>
 using namespace std;
 typedef int HPDataType;
@@ -8,4 +9,18 @@ typedef struct Heap
 	int _size;
 	int _capacity;
 }Heap;
-void HeapCreate(Heap* hp,)
+void AdjustDown(HPDataType* a, int size, int parents);
+//创建堆
+void HeapCreate(Heap* hp, HPDataType* a, int n);
+//堆的销毁
+void HeapDestory(Heap* hp);
+//删除堆顶元素
+void HeapPop(Heap* hp);
+
+
+//取堆顶元素
+HPDataType HeapTop(Heap* hp);
+//判断堆是否为空
+bool HeapEmpty(Heap* hp);
+//返回堆的大小
+int HeapSize(Heap* hp);
