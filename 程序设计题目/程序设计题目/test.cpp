56df7ad9ae3d81130311066c9fa066269199b90b
若鱼不是鱼 <21231270@bjtu.edu.cn>
2023-12-11 10:46:06 +0800

    程序设计题目代码

--- 程序设计题目/程序设计题目/test.cpp
+++ 程序设计题目/程序设计题目/test.cpp
@@ -1,37 +1,68 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <iostream>
+#include <utility>
 #include <vector>
 #include <algorithm>
+#include <cstdlib>
 using namespace std;
-
+const int N = 1000010;
+int a[N];
+struct prev
+{
+    prev()
+    {
+        flag = 0;
+        pos = 0;
+        val = 0;
+    }
+    int val;
+    int pos;
+    int flag;
+};
 int main() {
     int n;
-    cin >> n;
-
-    vector<int> daily_scores(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> daily_scores[i];
+    scanf("%d", &n);
+    vector<pair<int, int>> res;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        scanf("%d", &x);
+        res.push_back(pair<int, int>(x, i));
     }
-
-    // Sort the array of daily scores in ascending order
-    sort(daily_scores.begin(), daily_scores.end());
-
-    vector<int> final_scores(n);
-
-    for (int i = 0; i < n; ++i) {
-        if (i % 2 == 0) {
-            final_scores[i] = i / 2 + 1;  // Assign the minimum possible positive score
+    sort(res.begin(), res.end());
+    int tag = 1;
+    struct prev temp;
+    for (int i = 0; i < n; i++)
+    {
+        if (temp.flag == 0)
+        {
+            a[res[i].second] = tag;
+            temp.flag = 1;
         }
-        else {
-            final_scores[i] = n - i / 2;  // Assign the next higher score than previous
-        }
-    }
+        else
+        {
+            if (abs(res[i].second - temp.pos) == 1)
+            {
+                if (res[i].first == temp.val)
+                {
+                    a[res[i].second] = tag;
+                }
+                else
+                {
+                    tag++;
+                    a[res[i].second] = tag;
+                }
+            }
+            else
+            {
+                a[res[i].second] = tag;
 
-    // Output the array of final scores
-    for (int i = 0; i < n; ++i) {
-        cout << final_scores[i] << " ";
+            }
+        }
+        temp.pos = res[i].second;
+        temp.val = res[i].first;
     }
+    for (int i = 0; i < n; i++)cout << a[i] << " ";
     cout << endl;
-
     return 0;
 }
