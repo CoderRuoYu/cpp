7e1cef824d54ec9c222c5558194245fb1157d493
若鱼不是鱼 <21231270@bjtu.edu.cn>
2023-12-16 10:21:19 +0800

    stl-priority_queue
    stl中priority_queue的实现

---
+++ STL/priority_queue/priority_queue/Priority_queue.h
@@ -0,0 +1,91 @@
+#pragma once
+namespace gao
+{
+	template <class T, class Container = vector<T>, class Compare = less<T>>
+	class priority_queue
+	{
+	public:
+		priority_queue()
+		{
+			;
+		}
+		template <class InputIterator>
+		priority_queue(InputIterator first, InputIterator last)
+			:c(first,last)
+		{
+			//向下调整
+			if (c, size() > 1)
+			{
+				for (int i = (c.size() - 1 - 1) / 2; i >= 0; i--)
+				{
+					AdjustDown(i);
+				}
+			}
+		}
+		bool empty()const
+		{
+			return c.empty();
+		}
+		size_t size()const
+		{
+			return c.size();
+		}
+		T& top()
+		{
+			return c[0];
+		}
+		void push(const T& x)
+		{
+			c.push_back(x);
+			//向上调整
+			AdjustUp(c.size() - 1);
+		}
+		void pop()
+		{
+			swap(c[0], c[c.size() - 1]);
+			c.pop_back();
+			AdjustDown(0);
+		}
+	private:
+		void AdjustDown(int parent)
+		{
+			int child = 2 * parent + 1;
+			while (child < c.size())
+			{
+				if (child + 1 < c.size() && c[child] < c[child + 1])
+				{
+					child = child + 1;
+				}
+				if (c[parent] < c[child])
+				{
+					swap(c[parent], c[child]);
+				}
+				else
+				{
+					break;
+				}
+				parent = child;
+				child = parent * 2 + 1;
+			}
+		}
+		void AdjustUp(int child)
+		{
+			int parent = (child - 1) / 2;
+			while (parent >= 0)
+			{
+				if (c[child] > c[parent])
+				{
+					swap(c[child], c[parent]);
+				}
+				else
+				{
+					break;
+				}
+				child = parent;
+				parent = (child - 1) / 2;
+			}
+		}
+		Container c;
+		Compare comp;
+	};
+}
---
+++ STL/priority_queue/priority_queue/test.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+using namespace std;
+#include <iostream>
+#include <vector>
+#include <functional>
+#include "Priority_queue.h"
+void test01()
+{
+	gao::priority_queue<int> t;
+	cout << t.empty() << endl;
+	cout << t.size() << endl;
+}
+void test02()
+{
+	int arr[] = { 8,5,7,3,6,9,12,20 };
+	gao::priority_queue<int> t(arr, arr + sizeof(arr) / sizeof(arr[0]));
+	t.push(-44);
+	t.push(44);
+	while (!t.empty())
+	{
+		cout << t.top() << ' ';
+		t.pop();
+	}
+	
+}
+int main()
+{
+	test02();
+
+	return 0;
+}
