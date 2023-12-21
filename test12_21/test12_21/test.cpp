5b9e132edd92dac0e6cdb6c78fc6fa8559d04dc3
若鱼不是鱼 <21231270@bjtu.edu.cn>
2023-12-21 22:31:44 +0800

    测试代码

--- test12_21/test12_21/test.cpp
+++ test12_21/test12_21/test.cpp
@@ -1,42 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <iostream>
 using namespace std;
-class A
-{
-public:
-	void A_test()
-	{
-		cout << "A_test" << endl;
-	}
-	virtual ~A()
-	{
-		cout << "~A" << endl;
-	}
-};
-class B:public A
-{
-public:
 
-	~B()
-	{
-		cout << "~B" << endl;
-	}
-};
-class C:public B
-{
-public:
-
-	~C()
-	{
-		cout << "~C" << endl;
-	}
-};
 int main()
 {
-	A* test;
-	test = new C;
-	test->A_test();
-	delete test;
 
+	
 	return 0;
 }
