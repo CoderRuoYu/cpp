b4c2d0777badedfa14364efc38b4740002d7e081
若鱼不是鱼 <21231270@bjtu.edu.cn>
2023-12-28 16:09:17 +0800

    程序设计模式

--- 程序设计模式/代理模式/test.cpp
+++ 程序设计模式/代理模式/test.cpp
@@ -1,8 +1,46 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
 using namespace std;
+//懒汉式
+//class Factory
+//{
+//public:
+//	static Factory* GetInstance()
+//	{
+//		if (a == nullptr)
+//		{
+//			a = new Factory();
+//		}
+//		return a;
+//	}
+//private:
+//
+//	Factory()
+//	{
+//	}
+//	static Factory* a;
+//};
+//Factory* Factory::a = nullptr;
+//饥汉式
+class Factory
+{
+public:
+	static Factory* GetInstance()
+	{
+		return a;
+	}
+private:
+
+	Factory()
+	{
+	}
+	static Factory* a;
+};
+Factory* Factory::a = new Factory();
 int main()
 {
-
+	Factory* test = Factory::GetInstance();
+	Factory* test1 = Factory::GetInstance();
+	cout << test << endl << test1 << endl;
 	return 0;
 }
