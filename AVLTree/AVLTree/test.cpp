9d3298ccc570007125ea2afaf648f8aad0a449b6
若鱼不是鱼 <21231270@bjtu.edu.cn>
2024-04-10 20:22:17 +0800

    avl树的实现

--- AVLTree/AVLTree/test.cpp
+++ AVLTree/AVLTree/test.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void test01()
 {
 	AVLTree<int, int> t;
-	int arr[] = { 8,4,5,6,2,4,5,9,10,22,-1 };
+	int arr[] = { 8,4,5,6,2,4,5,9,10,22,-1 ,10000,20000,30000,50000,-2,-3,-6,-7,-8};
 	for (auto e : arr)
 	{
 		t.insert(make_pair(e, e));
