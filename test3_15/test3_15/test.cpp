9c90fb3ce022c279a1377a435ece9ab4b822e346
若鱼不是鱼 <21231270@bjtu.edu.cn>
2024-05-08 21:23:28 +0800

    测试代码

---
+++ test3_15/test3_15/test.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << (-13 % 12) << endl;
+
+	cout << (-3 % 12) << endl;
+	cout << 9 % 12 << endl;
+
+
+	return 0;
+}
